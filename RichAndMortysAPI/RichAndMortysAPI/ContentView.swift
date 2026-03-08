bf2b345646a64f8e73b2e8e3360d80b0e1616e98
Valentino <AL03077250@tecmilenio.mx>
2026-03-08 00:34:36 -0600

    Proyect init

---
+++ RichAndMortysAPI/RichAndMortysAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RichAndMortysAPI
+//
+//  Created by Valentino De Paola Gallardo on 08/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RichAndMortysAPI/RichAndMortysAPI/RichAndMortysAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichAndMortysAPIApp.swift
+//  RichAndMortysAPI
+//
+//  Created by Valentino De Paola Gallardo on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RichAndMortysAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
